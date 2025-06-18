4a45a03f837cba01054d543b800279497a7407eb
Xu Han <xu.han@outlook.com>
2025-06-18 10:23:06 +0800

    rename duplicate function name.

--- reproducers/02_sizeof_zero_size_array.cpp
+++ reproducers/02_sizeof_zero_size_array.cpp
@@ -1,6 +1,6 @@
 #include <cstdint>
 
-void repo_init_zero_size_array()
+void repo_sizeof_zero_size_array()
 {
     static constexpr int64_t int_array_2[] = {};
     int i_size = sizeof(int_array_2);
