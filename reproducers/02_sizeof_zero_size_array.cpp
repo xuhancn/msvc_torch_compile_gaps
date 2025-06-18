ca3e63a305b7585bf0ce13367302629a335a1f2e
Han, Xu <xu.han@intel.com>
2025-06-18 12:20:40 +0800

    add print size.

--- reproducers/02_sizeof_zero_size_array.cpp
+++ reproducers/02_sizeof_zero_size_array.cpp
@@ -1,7 +1,9 @@
 #include <cstdint>
+#include <stdio.h>
 
 void repo_sizeof_zero_size_array()
 {
     static constexpr int64_t int_array_2[] = {};
     int i_size = sizeof(int_array_2);
+    printf("Zero array size is: %d.\n", i_size);
 }
